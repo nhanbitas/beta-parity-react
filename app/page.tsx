7097de2727d24cd59549347a339465f553ee28fb
nhanbitas <nhandev.bitas@gmail.com>
2024-05-27 10:20:58 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import AccordionDemo from '@components/demo/AccordionDemo';
-import BadgeDemo from '@components/demo/BadgeDemo';
-import Header from '@components/doc/Header';
+import BadgeDemo from '@components/demo/BadgeDemo';;
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Header />
       <AccordionDemo />
       <BadgeDemo />
     </main>
