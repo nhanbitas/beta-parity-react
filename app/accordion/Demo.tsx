4a4d199b705827776b0d6d366264252b497bbbb3
nhanbitas <nhandev.bitas@gmail.com>
2024-07-15 16:49:11 +0700

    feat: accordion - no longer extends from radix - add kind, type, iconSide features

--- app/accordion/Demo.tsx
+++ app/accordion/Demo.tsx
@@ -1,17 +1,134 @@
 'use client';
 
-import { Accordion } from '@libComponents/Accordion';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@libComponents/Accordion';
+import { Plus } from 'lucide-react';
 import React from 'react';
 
 type Props = {};
 
 export const DemoSingle = (props: Props) => (
-  <Accordion type='single' collapsible onValueChange={(value) => console.log(value)} items={accordionItems} />
+  <>
+    <Accordion defaultValue='item-2' onValueChange={(value) => console.log(value)}>
+      <AccordionItem value='item-1'>
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-2'>
+        <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-3'>
+        <AccordionTrigger>Can it be animated?</AccordionTrigger>
+        <AccordionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore
+          magna aliqua.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+
+    <Accordion kind='flush' defaultValue='item-2' onValueChange={(value) => console.log(value)}>
+      <AccordionItem value='item-1'>
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-2'>
+        <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value='item-3'>
+        <AccordionTrigger>Can it be animated?</AccordionTrigger>
+        <AccordionContent>Yes! You can animate the Accordion with CSS or JavaScript.</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  </>
 );
 
-export const DemoMulti = (props: Props) => (
-  <Accordion type='multiple' onValueChange={(value) => console.log(value)} items={accordionItems} />
-);
+export const DemoMulti = (props: Props) => {
+  const [items, setItems] = React.useState(['item-3', 'item-2', 'item-1']);
+  return (
+    <>
+      <Accordion defaultValue={['item-2']} type='multiple' onValueChange={(value) => console.log(value)}>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+          <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Can it be animated?</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore
+            magna aliqua.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <Accordion kind='flush' value={items} type='multiple' onValueChange={(value) => console.log(value)}>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+          <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Can it be animated?</AccordionTrigger>
+          <AccordionContent>Yes! You can animate the Accordion with CSS or JavaScript.</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+};
+
+export const DemoIconLeft = (props: Props) => {
+  const [items, setItems] = React.useState(['item-3', 'item-2', 'item-1']);
+  return (
+    <>
+      <Accordion iconSide='left' defaultValue={['item-2']} onValueChange={(value) => console.log(value)}>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+          <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Can it be animated?</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlabore et dolore
+            magna aliqua.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <Accordion
+        kind='flush'
+        iconSide='left'
+        triggerIcon={<Plus />}
+        value={items}
+        onValueChange={(value) => console.log(value)}
+      >
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+          <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Can it be animated?</AccordionTrigger>
+          <AccordionContent>Yes! You can animate the Accordion with CSS or JavaScript.</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+};
+
 export const accordionItems = [
   {
     value: 'item-1',
--- registry/src/components/Accordion/index.tsx
+++ registry/src/components/Accordion/index.tsx
@@ -1,12 +1,22 @@
 'use client';
 
 import React from 'react';
-import * as RadixAccordion from '@radix-ui/react-accordion';
 import classNames from 'classnames';
 import './index.css';
 import { ChevronDown } from 'lucide-react';
+import { createPolymorphicComponent, PolymorphicComponentProps } from '../Base/factory';
+import Base, { BaseProps } from '../Base';
+import useCombinedRefs from '../hooks/useCombinedRefs';
+import useCloneChildren from '../hooks/useCloneChildren';
 
-export interface AccordionProps {
+export interface AccordionProps extends BaseProps {
+  type?: 'single' | 'multiple';
+  kind?: 'flush' | 'contained';
+  triggerIcon?: React.ReactNode;
+  iconSide?: 'left' | 'right';
+  defaultValue?: string | string[];
+  value?: string | string[];
+  onValueChange?: (value: string | string[]) => void;
   items?: {
     title: string;
     content: React.ReactNode | React.FC;
@@ -16,64 +26,216 @@ export interface AccordionProps {
   }[];
 }
 
-const Accordion = React.forwardRef<
-  React.ElementRef<typeof RadixAccordion.Root>,
-  React.ComponentPropsWithoutRef<typeof RadixAccordion.Root> & AccordionProps
->(({ children, className, items, ...props }, forwardedRef) => {
-  if (!items || items.length === 0)
-    return (
-      <RadixAccordion.Root className={classNames('accordion', className)} {...props} ref={forwardedRef}>
-        {children}
-      </RadixAccordion.Root>
+export const Accordion = createPolymorphicComponent<'div', AccordionProps>(
+  <C extends React.ElementType = 'div'>(
+    {
+      component,
+      className,
+      children,
+      type = 'single',
+      kind = 'contained',
+      defaultValue,
+      value,
+      iconSide,
+      triggerIcon,
+      onValueChange,
+      ...props
+    }: PolymorphicComponentProps<C, AccordionProps>,
+    ref: React.Ref<any>
+  ) => {
+    const Component = component || ('div' as C);
+    const [currentValue, setCurrentValue] = React.useState<string | string[]>(value || defaultValue || []);
+
+    if (type === 'multiple') {
+    }
+
+    const handleChange = (value: string) => {
+      if (type === 'single') {
+        const newValue = currentValue === value ? '' : value;
+        setCurrentValue(newValue);
+        if (onValueChange) onValueChange(newValue);
+      } else if (type === 'multiple') {
+        const newValue =
+          currentValue.includes(value) && Array.isArray(currentValue)
+            ? currentValue.filter((item) => item !== value)
+            : [...currentValue, value];
+        setCurrentValue(newValue);
+        if (onValueChange) onValueChange(newValue);
+      }
+    };
+
+    const cloneChildren = useCloneChildren(
+      children,
+      { type, currentValue, handleChange, iconSide, kind, triggerIcon },
+      { recursive: true }
     );
 
-  return (
-    <RadixAccordion.Root className={classNames('accordion', className)} {...props} ref={forwardedRef}>
-      {items.map((item: any) => (
-        <AccordionItem value={item.value} key={item.key} disabled={item.disabled ?? false}>
-          <AccordionTrigger>{item.title}</AccordionTrigger>
-          <AccordionContent>{item.content}</AccordionContent>
-        </AccordionItem>
-      ))}
-      {children}
-    </RadixAccordion.Root>
-  );
-});
+    return (
+      <Base component={Component} className={classNames('accordion', className, kind)} ref={ref} {...props}>
+        {cloneChildren}
+      </Base>
+    );
+  }
+);
 
 Accordion.displayName = 'Accordion';
 
-const AccordionItem = React.forwardRef<
-  React.ElementRef<typeof RadixAccordion.Item>,
-  React.ComponentPropsWithoutRef<typeof RadixAccordion.Item>
->(({ children, className, ...props }, forwardedRef) => (
-  <RadixAccordion.Item className={classNames('accordion-item', className)} {...props} ref={forwardedRef}>
-    {children}
-  </RadixAccordion.Item>
-));
+export interface AccordionItemProps extends Omit<AccordionProps, 'value'> {
+  value: string;
+  isActive?: boolean;
+  disabled?: boolean;
+}
+
+export const AccordionItem = createPolymorphicComponent<'div', AccordionItemProps>(
+  <C extends React.ElementType = 'div'>(
+    {
+      component,
+      className,
+      children,
+      isActive,
+      disabled = false,
+      currentValue,
+      defaultValue,
+      value,
+      handleChange,
+      type = 'single',
+      kind = 'contained',
+      triggerIcon,
+      iconSide = 'right',
+      onValueChange,
+      ...props
+    }: PolymorphicComponentProps<C, AccordionItemProps>,
+    ref: React.Ref<any>
+  ) => {
+    const Component = component || ('div' as C);
+
+    const isExpanded = type === 'multiple' ? currentValue?.includes(value as string) : value === currentValue;
+
+    const cloneChildren = React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        const { ...rest } = child.props;
+        return React.cloneElement(child, {
+          ...rest,
+          isExpanded: isExpanded,
+          handleChange: () => handleChange && handleChange(value as string)
+        });
+      }
+    });
+
+    return (
+      <Base
+        component={Component}
+        className={classNames('accordion-item', className, { active: isExpanded })}
+        ref={ref}
+        {...props}
+      >
+        {cloneChildren}
+      </Base>
+    );
+  }
+);
 
 AccordionItem.displayName = 'AccordionItem';
 
-const AccordionTrigger = React.forwardRef<
-  React.ElementRef<typeof RadixAccordion.Trigger>,
-  React.ComponentPropsWithoutRef<typeof RadixAccordion.Trigger>
->(({ children, className, ...props }, forwardedRef) => (
-  <RadixAccordion.Trigger className={classNames('accordion-trigger', className)} {...props} ref={forwardedRef}>
-    {children}
-    <ChevronDown className='accordion-chevron' aria-hidden />
-  </RadixAccordion.Trigger>
-));
+export interface AccordionTriggerProps extends AccordionProps {}
+
+export const AccordionTrigger = createPolymorphicComponent<'button', AccordionTriggerProps>(
+  <C extends React.ElementType = 'button'>(
+    {
+      component,
+      className,
+      children,
+      isExpanded,
+      type = 'single',
+      kind = 'contained',
+      defaultValue,
+      currentValue,
+      handleChange,
+      value,
+      triggerIcon,
+      iconSide = 'right',
+      onValueChange,
+      ...props
+    }: PolymorphicComponentProps<C, AccordionTriggerProps>,
+    ref: React.Ref<any>
+  ) => {
+    const Component = component || ('button' as C);
+
+    const iconElement = triggerIcon ? triggerIcon : <ChevronDown size={16} />;
+
+    return (
+      <Base
+        component={Component}
+        className={classNames('accordion-trigger', className, {
+          'icon-left': iconSide === 'left',
+          'icon-right': iconSide === 'right'
+        })}
+        onClick={() => handleChange()}
+        ref={ref}
+        {...props}
+      >
+        {iconSide === 'left' && <span className='accordion-trigger-icon'>{iconElement}</span>}
+        <span className='accordion-trigger-text'>{children}</span>
+        {iconSide === 'right' && <span className='accordion-trigger-icon'>{iconElement}</span>}
+      </Base>
+    );
+  }
+);
 
 AccordionTrigger.displayName = 'AccordionTrigger';
 
-const AccordionContent = React.forwardRef<
-  React.ElementRef<typeof RadixAccordion.Content>,
-  React.ComponentPropsWithoutRef<typeof RadixAccordion.Content>
->(({ children, className, ...props }, forwardedRef) => (
-  <RadixAccordion.Content className={classNames('accordion-content', className)} {...props} ref={forwardedRef}>
-    <div className='accordion-content-text'>{children}</div>
-  </RadixAccordion.Content>
-));
+export interface AccordionContentProps extends AccordionProps {}
+
+export const AccordionContent = createPolymorphicComponent<'div', AccordionContentProps>(
+  <C extends React.ElementType = 'div'>(
+    {
+      component,
+      className,
+      children,
+      isExpanded,
+      value,
+      currentValue,
+      defaultValue,
+      handleChange,
+      triggerIcon,
+      iconSide = 'right',
+      type = 'single',
+      kind = 'contained',
+      onValueChange,
+      ...props
+    }: PolymorphicComponentProps<C, AccordionContentProps>,
+    ref: React.Ref<any>
+  ) => {
+    const Component = component || ('div' as C);
+    const elementRef = React.useRef<HTMLDivElement>(null);
+    const combinedRef = useCombinedRefs(elementRef, ref);
+
+    const [maxHeight, setMaxHeight] = React.useState(
+      isExpanded ? elementRef.current && elementRef.current.scrollHeight + 24 : 0
+    );
+
+    React.useEffect(() => {
+      if (elementRef.current) {
+        // 24px for added padding of Accordion content
+        const newMaxHeight = isExpanded ? elementRef.current.scrollHeight + 24 : 0;
+        setMaxHeight(newMaxHeight);
+      }
+    }, [isExpanded]);
+
+    return (
+      <Base
+        component={Component}
+        style={{ maxHeight }}
+        className={classNames('accordion-content', className)}
+        ref={combinedRef}
+        {...props}
+      >
+        {children}
+      </Base>
+    );
+  }
+);
 
 AccordionContent.displayName = 'AccordionContent';
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+/// fix lại đệ quy clone, chỉ pass các props cần thiết cho từng component
