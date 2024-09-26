6bde0d91f2feda3865f349dfae53ece3bb6df346
nhanbitas <nhandev.bitas@gmail.com>
2024-09-26 09:24:14 +0700

    update (dot, badge, corner indicator): add js doc for types

--- registry/src/components/CornerIndicator/index.tsx
+++ registry/src/components/CornerIndicator/index.tsx
@@ -4,19 +4,88 @@ import './index.css';
 import Badge, { BadgeProps } from '../Badge';
 import Dot, { DotProps } from '../Dot';
 
+// =========================
+// Corner Indicator
+// =========================
+// Declare and export Corner Indicator type and Corner Indicator component
+
 export interface CornerIndicatorProps extends React.HTMLAttributes<HTMLDivElement> {
-  children?: string | React.ReactNode;
-  className?: string;
+  /**
+   * The label text displayed inside the corner indicator.
+   * If the label is undefined, it will return type "dot" for corner indicator
+   * @memberof CornerIndicatorProps
+   */
   label?: string;
+
+  /**
+   * Icon to be displayed inside the corner indicator for type "badge".
+   * @memberof CornerIndicatorProps
+   */
   icon?: React.ReactNode;
+
+  /**
+   * If true, an outline will be applied to the corner indicator.
+   * @default false
+   * @memberof CornerIndicatorProps
+   */
   outline?: boolean;
+
+  /**
+   * If true, the corner indicator will have a pulsing animation, for type "dot".
+   * @default false
+   * @memberof CornerIndicatorProps
+   */
   pulse?: boolean;
+
+  /**
+   * Offset from the corner of the container in pixels.
+   * Defines how far the indicator is placed from the corner.
+   * If offset larger than 0, the corner indicator is translated to outside
+   * @memberof CornerIndicatorProps
+   */
   offset?: number;
+
+  /**
+   * If true, the corner indicator will be displayed.
+   * @default false
+   * @memberof CornerIndicatorProps
+   */
   disable?: boolean;
+
+  /**
+   * Position of the corner indicator relative to the container.
+   * @default 'top-right'
+   * @memberof CornerIndicatorProps
+   */
   position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+
+  /**
+   * Color of the corner indicator.
+   * @default 'gray'
+   * @memberof CornerIndicatorProps
+   */
   color?: 'gray' | 'orange' | 'violet' | 'green' | 'red' | 'yellow' | 'blue' | 'lime' | 'cyan' | '';
+
+  /**
+   * Size of the corner indicator.
+   * "lg" size is just possible for the dot corner indicator
+   * @default 'md'
+   * @memberof CornerIndicatorProps
+   */
   size?: 'md' | 'sm' | 'xs' | 'lg';
+
+  /**
+   * The variant of the corner indicator.
+   * @default 'filled'
+   * @memberof CornerIndicatorProps
+   */
   variant?: 'filled' | 'outlined' | 'glass' | '';
+
+  /**
+   * Additional properties for the indicator BadgeProps | DotProps.
+   * Combines certain properties from BadgeProps and DotProps, excluding 'size', 'color', and 'variant'.
+   * @memberof CornerIndicatorProps
+   */
   indicatorProps?: Omit<BadgeProps, 'size' | 'color' | 'variant'> & Omit<DotProps, 'size' | 'color' | 'pulse'>;
 }
 
@@ -46,7 +115,7 @@ export const CornerIndicator = React.forwardRef<HTMLDivElement, CornerIndicatorP
 
     return (
       <div
-        className={classNames('corner-indicator-container', { 'with-border': outline }, className, position)}
+        className={classNames('corner-indicator-container', { bordered: outline }, className, position)}
         ref={ref}
         style={{ ...offsetValue }}
         {...props}
--- registry/src/components/Dot/index.tsx
+++ registry/src/components/Dot/index.tsx
@@ -2,9 +2,31 @@ import React from 'react';
 import './index.css';
 import classNames from 'classnames';
 
+// =========================
+// Dot
+// =========================
+// Declare and export Dot type and Dot component
+
 export interface DotProps extends React.HTMLAttributes<HTMLSpanElement> {
+  /**
+   * The size of the dot.
+   * @default 'md'
+   * @memberof DotProps
+   */
   size?: 'xs' | 'sm' | 'md' | 'lg';
+
+  /**
+   * Whether the dot has a pulsing animation.
+   * @default false
+   * @memberof DotProps
+   */
   pulse?: boolean;
+
+  /**
+   * The color of the dot.
+   * @default 'gray'
+   * @memberof DotProps
+   */
   color?: 'gray' | 'orange' | 'violet' | 'green' | 'red' | 'yellow' | 'blue' | 'lime' | 'cyan';
 }
 
