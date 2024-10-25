51ba2c7aaa54aa09d100c450c5066555d9a669e0
nhanbitas <nhandev.bitas@gmail.com>
2024-10-25 17:05:18 +0700

    fix (number input): add controlled behavior - add controlled behavior, overwrite react number format - fix min max handler - add jsdoc/comments

--- app/number-input/Demo.tsx
+++ app/number-input/Demo.tsx
@@ -3,20 +3,27 @@
 import React from 'react';
 import { NumberInput } from '@libComponents/NumberInput';
 
-type Props = {};
+type Props = any;
 
 export const DemoNumberInput = (props: Props) => {
+  const [value, setValue] = React.useState((props.value as number) ?? 0);
+  const handleChange = (values: any) => {
+    setValue(values.floatValue);
+    console.log(values);
+  };
+
   return (
-    <NumberInput
-      onValueChange={(values) => {
-        console.log(values);
-      }}
-      thousandSeparator='.'
-      decimalSeparator=','
-      wrapperProps={{ className: '!w-64' }}
-      onClear={() => console.log('clear')}
-      onUnitChange={(unit: string) => console.log(unit)}
-      {...props}
-    />
+    <div>
+      <NumberInput
+        value={value}
+        onValueChange={handleChange}
+        thousandSeparator='.'
+        decimalSeparator=','
+        wrapperProps={{ className: '!w-64' }}
+        onClear={() => console.log('clear')}
+        onUnitChange={(unit: string) => console.log(unit)}
+        {...props}
+      />
+    </div>
   );
 };
--- registry/src/components/NumberInput/index.tsx
+++ registry/src/components/NumberInput/index.tsx
@@ -2,19 +2,59 @@
 
 import * as React from 'react';
 import './index.css';
-import { NumericFormat, NumericFormatProps, SourceInfo } from 'react-number-format';
+import { NumericFormat, NumericFormatProps, numericFormatter, SourceInfo } from 'react-number-format';
 import { InputProps, Input } from '../BaseInput';
 import { ChevronDown, ChevronUp, MinusIcon, PlusIcon } from 'lucide-react';
 import classNames from 'classnames';
 
 export interface NumberInputProps extends Omit<InputProps, 'onChange'> {
+  /**
+   * The unit or list of units displayed alongside the input value.
+   * Can be a single string or an array of strings to select from.
+   *
+   * @memberof NumberInputProps
+   */
   unit?: string | string[];
+
+  /**
+   * Callback function triggered when the unit is changed.
+   *
+   * @param {string} unit - The new selected unit.
+   * @memberof NumberInputProps
+   */
   onUnitChange?: (unit: string) => void;
+
+  /**
+   * The minimum allowed value for the input.
+   *
+   * @memberof NumberInputProps
+   */
   min?: number;
+
+  /**
+   * The maximum allowed value for the input.
+   *
+   * @memberof NumberInputProps
+   */
   max?: number;
-  stepper?: 'auto' | 'chevron' | 'cross';
+
+  /**
+   * Type of stepper control to display for adjusting the input value.
+   * - `'auto'`: Automatic increment/decrement based on user interactions.
+   * - `'chevron'`: Chevron-style increment/decrement buttons.
+   * - `'separate'`: Cross-style button allowing put left and rigt buttons particularly.
+   *
+   * @memberof NumberInputProps
+   */
+  stepper?: 'auto' | 'chevron' | 'separate';
+
+  /**
+   * The increment or decrement value for the stepper control.
+   * Defaults to 1 if not specified.
+   *
+   * @memberof NumberInputProps
+   */
   stepControl?: number;
-  format?: any;
 }
 
 export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps & NumericFormatProps>(
@@ -38,32 +78,32 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps &
     },
     ref
   ) => {
-    const [currentValue, setCurrentValue] = React.useState(value || defaultValue || '');
+    const isControlled = value !== undefined;
+    const [currentValue, setCurrentValue] = React.useState(value ?? defaultValue ?? '');
 
     const handleChange = (values: any, event?: SourceInfo) => {
       let newValue = values.floatValue;
+      if (currentValue === newValue) return;
 
-      if (min !== undefined && newValue <= min) {
-        newValue = min;
-      } else if (max !== undefined && newValue >= max) {
-        newValue = max;
-      }
-
-      setCurrentValue(newValue);
+      // fix uncontrolled react-format-number
+      // if having value property that provided by HOC, we just dispatch event
+      // if value is not provided by HOC, we need update inner value - current value
+      if (!isControlled) setCurrentValue(newValue);
+      onValueChange?.({ floatValue: newValue, ...values }, { event } as SourceInfo);
       onChange?.({ target: { value: newValue } } as React.ChangeEvent<HTMLInputElement>);
-      onValueChange?.(newValue, { event } as SourceInfo);
     };
 
+    // overwrite clear function because react-format-number replace original input value and control value
     const handleClear = () => {
       onClear?.();
-      handleChange({ floatValue: '' });
+      handleChange({ floatValue: '', value: '', floatValueAsString: '' });
     };
 
+    // generate unit selector
     const isSelectUnit = Array.isArray(unit);
     const handleUitChage = (e: React.ChangeEvent<HTMLSelectElement>) => {
       onUnitChange?.(e.target.value as string);
     };
-
     const unitElement = !!unit ? (
       <span className={`input-icon number-input-unit ${isSelectUnit ? 'selectable' : ''}`}>
         {isSelectUnit ? (
@@ -80,19 +120,32 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps &
       </span>
     ) : null;
 
+    // define needed props for input and stepper
     const inputProps = { ...props, ref, value: currentValue, onClear: handleClear } as any;
-
     const stepperProps = {
-      value: isNaN(currentValue as number) ? 0 : +currentValue,
+      value: isNaN(currentValue as number) || currentValue === undefined ? 0 : +currentValue,
       min: min,
       max: max,
       hanldeValue: handleChange,
       stepControl: stepControl,
-      disabled: props.disabled || props.readOnly
+      disabled: props.disabled || props.readOnly,
+      formatProps: {
+        thousandSeparator: props.thousandSeparator,
+        decimalSeparator: props.decimalSeparator,
+        allowedDecimalSeparators: props.allowedDecimalSeparators,
+        thousandsGroupStyle: props.thousandsGroupStyle,
+        decimalScale: props.decimalScale,
+        fixedDecimalScale: props.fixedDecimalScale,
+        allowNegative: props.allowNegative,
+        allowLeadingZeros: props.allowLeadingZeros,
+        suffix: props.suffix,
+        prefix: props.prefix
+      } as FormatProps
     } as StepperProps;
 
+    // define left elements, it is depend on related props
     const renderLeftElement = (originalElement?: React.ReactNode) => {
-      if (stepper === 'cross') {
+      if (stepper === 'separate') {
         return (
           <>
             {originalElement}
@@ -103,6 +156,7 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps &
       return inputProps.leftIcon ? <span className='input-icon'>{inputProps.leftIcon}</span> : undefined;
     };
 
+    // define right elements, it is depend on related props
     const renderRightElement = (originalElement?: React.ReactNode) => {
       switch (stepper) {
         case 'auto':
@@ -119,7 +173,7 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps &
               <NumberButtonStepper {...stepperProps} isChervon />
             </>
           );
-        case 'cross':
+        case 'separate':
           return (
             <>
               {originalElement}
@@ -141,20 +195,19 @@ export const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps &
         customInput={Input}
         className={classNames('number-input', className, { [`${stepper}-stepper`]: !!stepper })}
         getInputRef={ref}
-        value={currentValue}
         onValueChange={handleChange}
-        // isAllowed is a pre handle function that returns true if the value is allowed
         isAllowed={({ floatValue }) => {
-          const isValidValue =
-            floatValue === undefined ||
-            ((min === undefined || floatValue >= min) && (max === undefined || floatValue <= max));
+          // TODO: fix case having value but onValueChange not change value - backlog
+          if (isControlled && !onValueChange) return false;
+          if (floatValue === undefined || floatValue.toString() === '') return true;
+          const isValidValue = (min === undefined || floatValue >= min) && (max === undefined || floatValue <= max);
           return isValidValue;
         }}
         wrapperProps={{
           ...inputProps.wrapperProps,
           leftElement: renderLeftElement(inputProps.wrapperProps?.leftElement)
-          // No overwrite right element because of base input features, use ActionBtn instead
         }}
+        // No overwrite right element because of base input features, use ActionBtn instead
         ActionBtn={renderRightElement(inputProps.ActionBtn)}
       />
     );
@@ -166,15 +219,30 @@ NumberInput.displayName = 'NumberInput';
 // =========================
 // Stepper
 // =========================
+
+type FormatProps = Pick<
+  NumericFormatProps,
+  | 'thousandSeparator'
+  | 'decimalSeparator'
+  | 'allowedDecimalSeparators'
+  | 'thousandsGroupStyle'
+  | 'decimalScale'
+  | 'fixedDecimalScale'
+  | 'allowNegative'
+  | 'allowLeadingZeros'
+  | 'suffix'
+  | 'prefix'
+>;
+
 interface StepperProps extends Pick<NumberInputProps, 'stepControl' | 'min' | 'max' | 'disabled'> {
   value: number;
   hanldeValue: (e: any) => void;
   minusOnly?: boolean;
   plusOnly?: boolean;
   isChervon?: boolean;
+  formatProps?: FormatProps;
 }
 
-// TODO: fix double dispatch events
 const NumberButtonStepper = ({
   value,
   hanldeValue,
@@ -184,12 +252,28 @@ const NumberButtonStepper = ({
   disabled = false,
   minusOnly = false,
   plusOnly = false,
-  isChervon = false
+  isChervon = false,
+  formatProps = {}
 }: StepperProps) => {
+  // create function handle value from number input, after that, render the suitable stepper button
   const handleChange = (type: '+' | '-') => {
     const stepValue = type === '+' ? stepControl : -stepControl;
-    const newValue = value + stepValue;
-    hanldeValue({ floatValue: isNaN(value) ? stepValue : newValue });
+    let newValue = value + stepValue;
+
+    // handle min and max when value is change
+    // if value larger than max, the new value is reset to max value
+    // if value lower than min, the new value is reset to min value
+    if (min !== undefined && newValue <= min) {
+      newValue = min;
+    } else if (max !== undefined && newValue >= max) {
+      newValue = max;
+    }
+
+    hanldeValue({
+      floatValue: isNaN(value) ? stepValue : newValue,
+      value: newValue.toString(),
+      formattedValue: numericFormatter(newValue.toString(), formatProps)
+    });
   };
 
   const minusDisable = disabled || (min !== undefined && value <= min) || isNaN(value);
