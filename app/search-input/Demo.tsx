a755756b309ed0a5524d6cc01affea038db53ce2
nhanbitas <nhandev.bitas@gmail.com>
2025-01-07 16:04:22 +0700

    draft (searchInput): add base

--- app/search-input/Demo.tsx
+++ app/search-input/Demo.tsx
@@ -1,14 +1,77 @@
 'use client';
 
+import { Settings2 } from 'lucide-react';
 import { SearchInput } from '@libComponents/SearchInput';
 import React from 'react';
+import { Menu, MenuItem } from '@libComponents/Menu';
 
 type Props = any;
+
+const searchItems = [
+  'Filter 1',
+  'Filter 2',
+  'Filter 3',
+  'Filter 4',
+  'Filter 5',
+  'Filter 6',
+  'Filter 7',
+  'Filter 8',
+  'Filter 9',
+  'Filter 10'
+] as const;
 
 export const DemoBasicSearchInput = (props: Props) => (
   <SearchInput
     wrapperProps={{ className: '!w-96', ...props.wrapperProps }}
     placeholder='Enter your search'
+    onSearch={() => console.log('Search')}
     {...props}
   />
 );
+
+export const DemoAuxiliarySearchInput = (props: Props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [values, setValues] = React.useState<string[]>([]);
+  const ref = React.useRef<any>(null);
+
+  const handleChange = (value: any) => {
+    if (!value) return;
+
+    if (values.includes(value)) {
+      setValues(values.filter((item) => item !== value));
+    } else {
+      setValues([...values, value]);
+    }
+
+    setIsOpen(!isOpen);
+  };
+
+  console.log(values);
+  return (
+    <>
+      <SearchInput
+        ref={ref}
+        auxiliaryActive={isOpen || (values && values.length > 0)}
+        wrapperProps={{ className: '!w-96', ...props.wrapperProps }}
+        placeholder='Enter your search'
+        onSearch={() => console.log('Search')}
+        onAuxiliaryAction={() => setIsOpen(!isOpen)}
+        auxiliaryIcon={<Settings2 />}
+        {...props}
+      />
+
+      <Menu
+        anchor={ref.current}
+        className='max-h-[200px] max-w-96'
+        size='sm'
+        isOpen={isOpen}
+        scrollIndicator={true}
+        prominence='pronounced'
+      >
+        {searchItems.map((item) => (
+          <MenuItem onChange={(e: any) => handleChange(e.target.value)} key={item} label={item} value={item} />
+        ))}
+      </Menu>
+    </>
+  );
+};
--- registry/src/components/SearchInput/index.tsx
+++ registry/src/components/SearchInput/index.tsx
@@ -2,40 +2,83 @@ import * as React from 'react';
 import './index.css';
 import './variables.css';
 import { Input } from '../BaseInput';
-import { Search } from 'lucide-react';
+import { Search, Settings2 } from 'lucide-react';
 import { Spinner } from '../Spinner';
+import { Button } from '../Button';
 
 export interface SearchInputProps extends React.ComponentPropsWithoutRef<typeof Input> {
   isPending?: boolean;
-  searchIconSide?: 'left' | 'right';
-  customAction?: React.ReactNode;
+  searchButton?: boolean;
+  searchButtonProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
+  auxiliaryIcon?: React.ReactNode;
+  auxiliaryActive?: boolean;
+  auxiliaryActionProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
+  onSearch?: () => void | any;
+  onAuxiliaryAction?: () => void | any;
 }
 
 export const SearchInput = React.forwardRef<React.ElementRef<typeof Input>, SearchInputProps>(
-  ({ type = 'search', isPending, searchIconSide = 'left', customAction, ...props }, ref) => {
-    const searchIcon = isPending ? (
-      <span className='input-icon'>
-        <Spinner size='sm' />
-      </span>
+  (
+    {
+      type = 'search',
+      isPending,
+      searchButton = false,
+      searchButtonProps,
+      auxiliaryIcon,
+      auxiliaryActive = false,
+      auxiliaryActionProps,
+      onSearch,
+      onAuxiliaryAction,
+      ...props
+    },
+    ref
+  ) => {
+    const searchIcon = isPending ? <Spinner size='sm' /> : <Search width={16} height={16} />;
+
+    const handleSearch = () => {
+      if (onSearch) {
+        onSearch();
+      }
+    };
+
+    const handleAuxiliaryAction = () => {
+      if (onAuxiliaryAction) {
+        onAuxiliaryAction();
+      }
+    };
+
+    const AuxiliaryActionElement = () => {
+      const TagName = (auxiliaryActive ? Button : 'button') as React.ElementType;
+      return (
+        <TagName
+          className='input-icon square-icon'
+          onClick={handleAuxiliaryAction}
+          {...(auxiliaryActive ? { color: 'neutral', kind: 'solid', size: 'sm' } : { type: 'button' })}
+          {...auxiliaryActionProps}
+        >
+          {auxiliaryIcon}
+        </TagName>
+      );
+    };
+
+    const inputDevider = searchButton || auxiliaryIcon !== undefined ? <span className='input-divider'></span> : null;
+    const rightSearchIcon = isPending ? (
+      <span className='input-icon'>{searchIcon}</span>
     ) : (
-      <button type='button' className='input-icon'>
-        <Search />
+      <button type='button' className='input-icon' onClick={handleSearch} {...searchButtonProps}>
+        {searchIcon}
       </button>
     );
+    const rightElement = (
+      <>
+        {inputDevider}
+        {searchButton && rightSearchIcon}
+        {auxiliaryIcon !== undefined && <AuxiliaryActionElement />}
+      </>
+    );
 
-    const customActionElement = customAction ? <span className='input-action'>{customAction}</span> : undefined;
-
-    const rightElement =
-      searchIconSide === 'right' ? (
-        <>
-          {searchIcon}
-          {customActionElement}
-        </>
-      ) : (
-        customActionElement
-      );
-
-    const leftElement = searchIconSide === 'left' ? searchIcon : undefined;
+    const leftSearchIcon = searchButton ? undefined : searchIcon;
+    const leftElement = leftSearchIcon;
 
     return <Input ref={ref} type={type} {...props} leftIcon={leftElement} ActionBtn={rightElement} />;
   }
