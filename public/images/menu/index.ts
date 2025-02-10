1117435df9d901c1e6bfca46cf1c8a70c5cfa1ef
nhanbitas <nhandev.bitas@gmail.com>
2025-02-10 17:07:59 +0700

    doc: update menu images

---
+++ public/images/menu/index.ts
@@ -0,0 +1,79 @@
+import accent from './accent.png';
+import alternative from './alternative.png';
+import anatomy2 from './anatomy-2.png';
+import anatomy3 from './anatomy-3.png';
+import anatomy from './anatomy.png';
+import checkboxInput from './checkbox-input.png';
+import defaultLayer from './default.png';
+import disabled from './disabled.png';
+import enabled from './enabled.png';
+import filterInput from './filter-input.png';
+import filterInput2 from './filter-input-2.png';
+import focused from './focused.png';
+import groupLabelOff from './group-label-off.png';
+import groupLabelOn from './group-label-on.png';
+import hidden from './hidden.png';
+import hovered from './hovered.png';
+import icon from './icon.png';
+import labelAlignment from './label-alignment.png';
+import leftsideCheckmark from './leftside-checkmark.png';
+import medium from './medium.png';
+import menu from './menu.png';
+import neutral from './neutral.png';
+import position from './position.png';
+import position2 from './position-2.png';
+import pronounced from './pronounced.png';
+import radioInput from './radio-input.png';
+import rightsideCheckmark from './rightside-checkmark.png';
+import scrollButton from './scroll-button.png';
+import selectedPronouncedHovered from './selected-pronounced-hovered.png';
+import selectedPronounced from './selected-pronounced.png';
+import selectedSubtleHovered from './selected-subtle-hovered.png';
+import selectedSubtle from './selected-subtle.png';
+import small from './small.png';
+import sublabel from './sublabel.png';
+import subtle from './subtle.png';
+import textOverflow from './text-overflow.png';
+import unselected from './unselected.png';
+import visible from './visible.png';
+
+export default {
+  accent,
+  alternative,
+  anatomy2,
+  anatomy3,
+  anatomy,
+  checkboxInput,
+  defaultLayer,
+  disabled,
+  enabled,
+  filterInput,
+  filterInput2,
+  focused,
+  groupLabelOff,
+  groupLabelOn,
+  hidden,
+  hovered,
+  icon,
+  labelAlignment,
+  leftsideCheckmark,
+  medium,
+  menu,
+  neutral,
+  position,
+  position2,
+  pronounced,
+  radioInput,
+  rightsideCheckmark,
+  scrollButton,
+  selectedPronouncedHovered,
+  selectedPronounced,
+  selectedSubtle,
+  selectedSubtleHovered,
+  small,
+  sublabel,
+  subtle,
+  textOverflow,
+  unselected,
+  visible
+};
