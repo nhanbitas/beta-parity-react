9a7441e715f2cfb80da6b6d50fe3738a902080b2
nhanbitas <nhandev.bitas@gmail.com>
2025-02-11 15:04:02 +0700

    doc: update password-input images

---
+++ public/images/password-input/index.ts
@@ -0,0 +1,73 @@
+import alternative from './alternative.png';
+import anatomy from './anatomy.png';
+import anatomy2 from './anatomy-2.png';
+import clearButton from './clear-button.png';
+import clearButton2 from './clear-button-2.png';
+import clearButtonOption from './clear-button-option.png';
+import disabled from './disabled.png';
+import disabled2 from './disabled-2.png';
+import empty from './empty.png';
+import empty2 from './empty-2.png';
+import enabled from './enabled.png';
+import enabled2 from './enabled-2.png';
+import filled from './filled.png';
+import filled2 from './filled-2.png';
+import floatingLabel from './floating-label.png';
+import focused from './focused.png';
+import focused2 from './focused-2.png';
+import hovered from './hovered.png';
+import hovered2 from './hovered-2.png';
+import icon from './icon.png';
+import invalid from './invalid.png';
+import invalid2 from './invalid-2.png';
+import layerDefault from './layer-default.png';
+import masked from './masked.png';
+import masked2 from './masked-2.png';
+import medium from './medium.png';
+import passwordInput from './password-input.png';
+import placeholder from './placeholder.png';
+import plaintext from './plaintext.png';
+import plaintext2 from './plaintext-2.png';
+import readonly from './readonly.png';
+import readonly2 from './readonly-2.png';
+import small from './small.png';
+import toggleVisibilityButton from './toggle-visibbility-button.png';
+import toggleVisibilityButton2 from './toggle-visibbility-button-2.png';
+
+export default {
+  alternative,
+  anatomy,
+  anatomy2,
+  clearButton,
+  clearButton2,
+  clearButtonOption,
+  disabled,
+  disabled2,
+  empty,
+  empty2,
+  enabled,
+  enabled2,
+  filled,
+  filled2,
+  floatingLabel,
+  focused,
+  focused2,
+  hovered,
+  hovered2,
+  icon,
+  invalid,
+  invalid2,
+  layerDefault,
+  masked,
+  masked2,
+  medium,
+  passwordInput,
+  placeholder,
+  plaintext,
+  plaintext2,
+  readonly,
+  readonly2,
+  small,
+  toggleVisibilityButton,
+  toggleVisibilityButton2
+};
