9ba33c6520c6ef6c72a78aae551d7ba88a20098b
nhanbitas <nhandev.bitas@gmail.com>
2025-02-13 15:57:36 +0700

    doc: update select images

---
+++ public/images/select/index.ts
@@ -0,0 +1,71 @@
+import alternative from './alternative.png';
+import anatomy from './anatomy.png';
+import anatomy2 from './anatomy-2.png';
+import anatomy3 from './anatomy-3.png';
+import clearButtonOption from './clear-button-option.png';
+import clearButton from './clear-button.png';
+import defaultLayer from './default-layer.png';
+import disabled from './disabled.png';
+import disabled2 from './disabled-2.png';
+import dropdownMenuOption from './dropdown-menu-option.png';
+import dropdownMenu from './dropdown-menu.png';
+import enabled from './enabled.png';
+import enabled2 from './enabled-2.png';
+import floatingLabel from './floating-label.png';
+import focused from './focused.png';
+import focused2 from './focused-2.png';
+import hovered from './hovered.png';
+import hovered2 from './hovered-2.png';
+import icon from './icon.png';
+import invalid from './invalid.png';
+import invalid2 from './invalid-2.png';
+import medium from './medium.png';
+import multipleSelection from './multiple-selection.png';
+import placeholder from './placeholder.png';
+import readonly from './readonly.png';
+import readonly2 from './readonly-2.png';
+import selectOptionTag from './select-option-tag.png';
+import select from './select.png';
+import selected from './selected.png';
+import selected2 from './selected-2.png';
+import small from './small.png';
+import tagOverflow from './tag-overflow.png';
+import unselected from './unselected.png';
+import unselected2 from './unselected-2.png';
+
+export default {
+  alternative,
+  anatomy,
+  anatomy2,
+  anatomy3,
+  clearButtonOption,
+  clearButton,
+  defaultLayer,
+  disabled,
+  disabled2,
+  dropdownMenuOption,
+  dropdownMenu,
+  enabled,
+  enabled2,
+  floatingLabel,
+  focused,
+  focused2,
+  hovered,
+  hovered2,
+  icon,
+  invalid,
+  invalid2,
+  medium,
+  multipleSelection,
+  placeholder,
+  readonly,
+  readonly2,
+  selectOptionTag,
+  select,
+  selected,
+  selected2,
+  small,
+  tagOverflow,
+  unselected,
+  unselected2
+};
