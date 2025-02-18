af98ba7bce7094d6df6d51997c5b9d2624b1fff7
nhanbitas <nhandev.bitas@gmail.com>
2025-02-18 15:19:35 +0700

    draft: progress

--- app/progress/@dev/demo-components.tsx
+++ app/progress/@dev/demo-components.tsx
@@ -1,1 +1,69 @@
+'use client';
+
+import React from 'react';
+import { Progress } from '@libComponents/Progress';
+
 // Define  demo components
+export function DemoBasicProgress(props: any) {
+  return <Progress {...props} />;
+}
+
+export function DemoSuccessProgress(props: any) {
+  const [progress, setProgress] = React.useState(0);
+  const [state, setState] = React.useState<'active' | 'success' | 'error'>('active');
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          clearInterval(interval);
+          setState('success');
+          return 100;
+        }
+        return prevProgress + 10;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Progress
+      title='File '
+      value={progress}
+      state={state}
+      helperText={state === 'success' ? 'Completed' : 'Uploading...'}
+      {...props}
+    />
+  );
+}
+
+export function DemoErrorProgress(props: any) {
+  const [progress, setProgress] = React.useState(0);
+  const [state, setState] = React.useState<'active' | 'success' | 'error'>('active');
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 87) {
+          clearInterval(interval);
+          setState('error');
+          return 87;
+        }
+        return prevProgress + 1;
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Progress
+      title='File '
+      value={progress}
+      state={state}
+      helperText={state === 'error' ? 'File is too large' : 'Uploading...'}
+      {...props}
+    />
+  );
+}
--- registry/src/components/Progress/index.tsx
+++ registry/src/components/Progress/index.tsx
@@ -1,12 +1,76 @@
 import * as React from 'react';
+import './variables.css';
 import './index.css';
-import { Input } from '../BaseInput';
+import classNames from 'classnames';
 
-export interface ProgressProps extends React.ComponentPropsWithoutRef<typeof Input> {}
+export interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
+  kind?: 'bar' | 'circle';
+  value?: number;
+  title?: string;
+  helperText?: string;
+  color?: 'neutral' | 'accent';
+  state?: 'active' | 'success' | 'error';
+}
 
-export const Progress = React.forwardRef<React.ElementRef<typeof Input>, ProgressProps>(
-  ({ type = 'file', ...props }, ref) => {
-    return <Input ref={ref} type={type} {...props} />;
+export const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(
+  ({ className, kind = 'bar', value = 0, title, helperText, color = 'neutral', state = 'active', ...props }, ref) => {
+    const clampedValue = Math.min(100, Math.max(1, value ?? 1));
+    const isCircle = kind === 'circle';
+
+    const renderProgress = () => {
+      if (isCircle) {
+        return (
+          <svg width={100} height={100} viewBox='0 0 100 100'>
+            <circle
+              className='progress-track'
+              cx={50}
+              cy={50}
+              r={40}
+              //  stroke='#e0e0e0'
+              strokeWidth={8}
+              fill='none'
+            />
+            <circle
+              className='progress-thumb'
+              cx={50}
+              cy={50}
+              r={40}
+              // stroke={thumbColor}
+              strokeWidth={9}
+              fill='none'
+              strokeDasharray='251.2'
+              strokeDashoffset={251.2 - (251.2 * clampedValue) / 100}
+              strokeLinecap='round'
+              transform='rotate(-90 50 50)'
+            />
+            <text className='progress-title' x={50} y={55} fontSize={14} textAnchor='middle'>
+              {`${clampedValue}%`}
+            </text>
+          </svg>
+        );
+      }
+
+      return (
+        <div className='progress-track'>
+          <div className='progress-thumb' style={{ width: `${clampedValue}%` }}></div>
+        </div>
+      );
+    };
+
+    return (
+      <div ref={ref} {...props} className={classNames('progress', kind, color, state, className)}>
+        {!isCircle && (
+          <div className='progress-title-wrapper'>
+            <span className='progress-title'>{title}</span>
+            <span className='progress-numeral'>{value}%</span>
+          </div>
+        )}
+
+        {renderProgress()}
+
+        <span className='progress-helper-text'>{helperText}</span>
+      </div>
+    );
   }
 );
 
